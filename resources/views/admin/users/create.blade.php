d747f1fa61190a27d493a41777405efd0a2f06ee
Andrei Mihailescu <andrei.mihailescu994@gmail.com>
2017-12-29 18:43:53 +0200

    Worked on create user form

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -4,4 +4,44 @@
 
     <h1>Create user</h1>
 
+    {!! Form::open(['method' => 'POST', 'action'=> 'AdminUsersController@store', 'files'=> true]) !!}
+
+    <div class="form-group">
+        {!! Form::label('name', 'Name:') !!}
+        {!! Form::text('name', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('email', 'Email:') !!}
+        {!! Form::email('email', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('role_id', 'Role:') !!}
+        {!! Form::select('role_id', ['' => 'Choose options'] + $roles, null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('is_active', 'Status:') !!}
+        {!! Form::select('is_active', [1 => 'Active', 0 => 'Not active'], 0, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('file', 'File:') !!}
+        {!! Form::file('file', null ) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('password', 'Password:') !!}
+        {!! Form::password('password', ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::submit('Create', ['class'=>'btn btn-primary']) !!}
+    </div>
+
+    {!! Form::close() !!}
+
+    @include('includes.form_error')
+
 @endsection
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -11,7 +11,7 @@
             <th>Name</th>
             <th>Email</th>
             <th>Role</th>
-            <th>Active</th>
+            <th>Status</th>
             <th>Created</th>
             <th>Updated</th>
         </tr>
@@ -26,7 +26,7 @@
                     <td>{{ $user->id }}</td>
                     <td>{{ $user->name }}</td>
                     <td>{{ $user->email }}</td>
-                    <td>{{ $user->role->name }}</td>
+                    <td>{{ ($user->role == null) ? 'User has no role' : $user->role->name }}</td>
                     <td>{{ $user->is_active == 1 ? 'Active' : 'Not active' }}</td>
                     <td>{{ $user->created_at->diffForHumans() }}</td>
                     <td>{{ $user->updated_at->diffForHumans() }}</td>
