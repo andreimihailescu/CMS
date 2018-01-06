7a91e5397547d57928cc0fcc4fdfa679254efbee
Andrei Mihailescu <andrei.mihailescu994@gmail.com>
2018-01-06 15:23:48 +0200

    Added delete post functionality

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -4,35 +4,55 @@
 
     <h1>Edit Post</h1>
 
+
+
     <div class="row">
 
-        {!! Form::model($post, ['method' => 'PATCH', 'action'=> ['AdminPostsController@update', $post->id], 'files'=> true]) !!}
-
-        <div class="form-group">
-            {!! Form::label('title', 'Title:') !!}
-            {!! Form::text('title', null, ['class' => 'form-control']) !!}
+        <div class="col-sm-3">
+            <img src="{{ $post->photo->file }}" alt="" class="img-responsive">
         </div>
 
-        <div class="form-group">
-            {!! Form::label('category_id', 'Category:') !!}
-            {!! Form::select('category_id', [''=> 'Choose categories'] + $categories, null, ['class' => 'form-control']) !!}
-        </div>
+        <div class="col-sm-9">
 
-        <div class="form-group">
-            {!! Form::label('photo_id', 'Photo:') !!}
-            {!! Form::file('photo_id') !!}
-        </div>
+            {!! Form::model($post, ['method' => 'PATCH', 'action'=> ['AdminPostsController@update', $post->id], 'files'=> true]) !!}
 
-        <div class="form-group">
-            {!! Form::label('body', 'Body:') !!}
-            {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
-        </div>
+            <div class="form-group">
+                {!! Form::label('title', 'Title:') !!}
+                {!! Form::text('title', null, ['class' => 'form-control']) !!}
+            </div>
 
-        <div class="form-group">
-            {!! Form::submit('Save', ['class'=>'btn btn-primary']) !!}
-        </div>
+            <div class="form-group">
+                {!! Form::label('category_id', 'Category:') !!}
+                {!! Form::select('category_id', [''=> 'Choose categories'] + $categories, null, ['class' => 'form-control']) !!}
+            </div>
 
-        {!! Form::close() !!}
+            <div class="form-group">
+                {!! Form::label('photo_id', 'Photo:') !!}
+                {!! Form::file('photo_id') !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('body', 'Body:') !!}
+                {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::submit('Save', ['class'=>'btn btn-primary col-sm-1', 'style'=> 'margin-right: 10px']) !!}
+            </div>
+
+            {!! Form::close() !!}
+
+
+
+            {!! Form::open(['method' => 'DELETE', 'action'=> ['AdminPostsController@destroy', $post->id]]) !!}
+
+            <div class="form-group">
+                {!! Form::submit('Delete', ['class'=>'btn btn-danger col-sm-1']) !!}
+            </div>
+
+            {!! Form::close() !!}
+
+        </div>
 
     </div>
 
