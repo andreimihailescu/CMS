5d0185224ce05909ebc14b9b0d7e7c50ed97d185
Andrei Mihailescu <andrei.mihailescu994@gmail.com>
2018-01-06 14:11:18 +0200

    Added categories to posts

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Http\Requests\PostsCreateRequest;
 use App\Photo;
 use App\Post;
@@ -21,7 +22,9 @@ class AdminPostsController extends Controller
 
     public function create()
     {
-        return view('admin.posts.create');
+        $categories = Category::lists('name', 'id')->all();
+
+        return view('admin.posts.create', compact('categories'));
     }
 
     public function store(PostsCreateRequest $request)
